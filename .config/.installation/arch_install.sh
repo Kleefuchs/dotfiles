e13deb15b3d43b23f9055935bc75adc15f7603af
Kleefuchs <Kleefuchs@outlook.de>
2025-10-17 05:14:14 +0200

    waybar2

---
+++ .config/.installation/arch_install.sh
@@ -0,0 +1,6 @@
+read -p "Enter command for installing packages like \"yay -S \":" pkg_manage_install
+
+$pkg_manage_install waybar dunst playerctl pavucontrol hyprpaper swww hyprshot hyprlock hyprswitch wpctl wl-kbptr wlrctl hyfetch zen-browser-bin mission-center brave-bin xfce4-thunar kitty hyprpolkitagent xdg-desktop-portal-hyprland brightnessctl wl-kbptr clipse-bin calc hyfetch hyprland neovim waybar-mpris-git cmake make jsonc yt-dlp kdeconnect adwaita-fonts adwaita-cursors adwaita-icon-theme adwaita-icon-theme-legacy libadwaita cmus swaylock ttf-jetbrains-mono-nerd wl-clipboard wshowkeys-mao-git nvidia_oc wireplumber walker-bin clipse-bin zoxide zsh wayshowkeys-mao-git
+
+bash install_waybar_scrolling_mpris.sh
+bash install_hyprswitcher.sh
---
+++ .config/.installation/install_waybar_scrolling_mpris.sh
@@ -0,0 +1,7 @@
+
+git clone https://github.com/sicista33/waybar-scrolling-mpris.git
+cd ./waybar-scrolling-mpris/build
+cmake ..
+make
+
+sudo mv waybar-scrolling-mpris ~/.config/waybar/waybar-scrolling-mpris
