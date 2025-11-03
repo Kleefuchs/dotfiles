da74db2164923b699d82d986908fd4464cd8f693
Kleefuchs <sam.brecht@outlook.de>
2025-11-03 06:42:30 +0100

    added eza and bat to install

--- .config/.installation/arch_install.sh
+++ .config/.installation/arch_install.sh
@@ -1,5 +1,5 @@
 read -p "Enter command for installing packages like \"yay -S \":" pkg_manage_install
 
-$pkg_manage_install waybar dunst playerctl pavucontrol swww wpctl hyfetch zen-browser-bin mission-center xfce4-thunar kitty brightnessctl clipse-bin calc hyfetch neovim cmake make jsonc yt-dlp kdeconnect adwaita-fonts adwaita-cursors adwaita-icon-theme adwaita-icon-theme-legacy libadwaita swaylock ttf-jetbrains-mono-nerd wl-clipboard wireplumber clipse-bin zoxide zsh fuzzel adw-gtk-theme
+$pkg_manage_install waybar dunst playerctl pavucontrol swww wpctl hyfetch zen-browser-bin mission-center xfce4-thunar kitty brightnessctl clipse-bin calc hyfetch neovim cmake make jsonc yt-dlp kdeconnect adwaita-fonts adwaita-cursors adwaita-icon-theme adwaita-icon-theme-legacy libadwaita swaylock ttf-jetbrains-mono-nerd wl-clipboard wireplumber clipse-bin zoxide zsh fuzzel adw-gtk-theme eza bat
 
 bash install_waybar_scrolling_mpris.sh
