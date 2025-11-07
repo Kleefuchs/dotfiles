59ab9c5378545a2d9be07280d71d58facc2400f6
Kleefuchs <sam.brecht@outlook.de>
2025-11-07 21:38:49 +0100

    fuzzel shit

--- .config/.installation/arch_install.sh
+++ .config/.installation/arch_install.sh
@@ -1,5 +1,7 @@
 read -p "Enter command for installing packages like \"yay -S \":" pkg_manage_install
 
-$pkg_manage_install waybar dunst playerctl pavucontrol swww wpctl hyfetch zen-browser-bin mission-center xfce4-thunar kitty brightnessctl clipse-bin calc hyfetch neovim cmake make jsonc yt-dlp kdeconnect adwaita-fonts adwaita-cursors adwaita-icon-theme adwaita-icon-theme-legacy libadwaita swaylock ttf-jetbrains-mono-nerd wl-clipboard wireplumber clipse-bin zoxide zsh fuzzel adw-gtk-theme eza bat
+$pkg_manage_install waybar dunst playerctl pavucontrol swww wpctl hyfetch zen-browser-bin mission-center xfce4-thunar kitty brightnessctl clipse-bin calc hyfetch neovim cmake make jsonc yt-dlp kdeconnect adwaita-fonts adwaita-cursors adwaita-icon-theme adwaita-icon-theme-legacy libadwaita swaylock ttf-jetbrains-mono-nerd wl-clipboard wireplumber clipse-bin zoxide zsh fuzzel adw-gtk-theme eza bat nvm
+
+nvm install stable
 
 bash install_waybar_scrolling_mpris.sh
