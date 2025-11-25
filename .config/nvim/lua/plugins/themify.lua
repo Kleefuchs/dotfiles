40c719d9f6ac562960bd3ecff529bd73195a082f
Kleefuchs <sam.brecht@outlook.de>
2025-11-25 15:48:19 +0100

    Configured themify correctly

--- .config/nvim/lua/plugins/themify.lua
+++ .config/nvim/lua/plugins/themify.lua
@@ -7,8 +7,54 @@ return {
     config = {
         -- Your list of colorschemes.
 
-        'folke/tokyonight.nvim',
-        'rose-pine/neovim',
+        {
+            'folke/tokyonight.nvim',
+            blacklist = {
+                'tokyonight-day'
+            }
+        },
+        {
+            'catppuccin/nvim',
+            blacklist = {
+                'catppuccin-latte'
+            }
+        },
+        {
+            'rose-pine/neovim',
+            whitelist = {
+                'rose-pine-moon',
+                'rose-pine-main'
+            },
+        },
+        {
+            'uhs-robert/oasis.nvim',
+            blacklist = {
+                'oasis-day',
+                'oasis-dawn',
+                'oasis-dawnlight',
+                'oasis-dusk',
+                'oasis-dust'
+            }
+        },
+        {
+            'morhetz/gruvbox',
+        },
+        {
+            'rebelot/kanagawa.nvim',
+            blacklist = {
+                'kanagawa-lotus'
+            }
+        },
+        {
+            'EdenEast/nightfox.nvim',
+            blacklist = {
+                'dayfox',
+                'dawnfox'
+            }
+        },
+        {
+            'daltonmenezes/aura-theme'
+        },
 
         -- Built-in colorschemes are also supported.
         -- (Also works with any colorschemes that are installed via other plugin manager, just make sure the colorscheme is loaded before Themify is loaded.)
@@ -16,7 +62,7 @@ return {
         loader = function()
             -- Custom loader logic...
             local Themify = require('themify.api')
-            Themify.set_current('rose-pine/neovim', 'rose-pine-moon')
+            Themify.set_current('EdenEast/nightfox.nvim', 'carbonfox')
         end,
     }
 }
